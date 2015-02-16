8060b2025cd82efb0b990a131efd84eb59f57282
Andy Montes <andymontes18@gmail.com>
2015-02-15 18:44:20 -0800

    Andy Montes Assignment 6 JUnit Ordering Test

--- src/test/java/edu/csupomona/cs480/AppTest.java
+++ src/test/java/edu/csupomona/cs480/AppTest.java
@@ -1,8 +1,15 @@
 package edu.csupomona.cs480;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import edu.csupomona.cs480.constructs.LunchboxManager;
 import edu.csupomona.cs480.data.Food;
 import edu.csupomona.cs480.data.FoodMap;
+import edu.csupomona.cs480.data.provider.FoodManager;
+import edu.csupomona.cs480.util.SortingManager;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -64,5 +71,37 @@ public class AppTest
             String UID2 = l.makeUniqueId();
             assertNotSame(UID1, UID2);
         }
+    }
+    
+    //Andy Montes
+    //Assignemnt 6
+    @Autowired
+    private FoodManager foodManager;
+    public void testSorting(){
+    	System.out.println("------List Size Test Not Same------");
+    	SortingManager sm = new SortingManager();
+    	List<Food> l1 = new ArrayList<Food>();
+    	List<Food> l2 = new ArrayList<Food>();
+    	
+    	Food f1 = new Food();
+        f1.setId("Food 1");
+        f1.setPrice("2.99");
+        Food f2 = new Food();
+        f2.setId("Food 2");
+        f2.setPrice("4.99");
+        
+        //different list 
+        l1.add(f1);
+        l1.add(f2);
+        
+        //different list
+        l2.add(f1);
+        l1.add(f2);
+        
+        //after sorting second list, they should not be the same
+        sm.highToLow(l2);
+       
+        assertNotSame(l1,l2);
+        
     }
 }
