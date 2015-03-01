f424613548944bc625946a75646370856911b027
Adamantcheese <random123x@gmail.com>
2015-03-01 14:43:06 -0800

    Reverting changes because it didn't work.

--- src/main/java/edu/csupomona/cs480/util/ResourceResolver.java
+++ src/main/java/edu/csupomona/cs480/util/ResourceResolver.java
@@ -8,12 +8,17 @@ import java.io.File;
 public class ResourceResolver {
 
     /**
+     * The base folder to store all the data used by this project.
+     */
+    private static final String BASE_DIR = System.getProperty("user.home") + "/cs480";
+
+    /**
      * Get the file used to store the user object JSON
      *
      * @return
      */
-    public static File getUserFile() {
-        File file = new File("src/main/resources/static/food-map-HASHMAP_VERSION.json");
+    public static File getUserFile () {
+        File file = new File(BASE_DIR + "/food-map-HASHMAP_VERSION.json");
         if (!file.getParentFile().exists()) {
             file.getParentFile().mkdirs();
         }
